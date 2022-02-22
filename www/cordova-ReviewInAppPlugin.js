e5aa47b4ce9bdd85dfe3897eaebe9243155a81fc
Jair Alexis Ortega Candamo <jairortegacandamo@gmail.com>
2022-02-22 11:48:17 -0500

    modificacion js

--- www/cordova-ReviewInAppPlugin.js
+++ www/cordova-ReviewInAppPlugin.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.requestReview = function (arg0, success, error) {
     exec(success, error, 'cordova-ReviewInAppPlugin', 'requestReview', [arg0]);
 };
+
+exports.requestReviewInApp = function () {
+    console.log("Todo bien");
+};
